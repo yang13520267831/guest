91f81f4595e9b8d23b30077d412d561d8b474840
yang13520267831 <yangpfnihao@163.com>
2020-08-11 14:20:54 +0800

    '08111420'

--- sign/urls.py
+++ sign/urls.py
@@ -8,6 +8,10 @@
 
 from django.conf.urls import url
 from sign import views_if
+
+app_name='sign'
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    # ex:/api/add_event/
+
+    url(r'^add_event/', views_if.add_event, name='add_event'),
 ]
--- sign/views.py
+++ sign/views.py
@@ -7,8 +7,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
+
 # Create your views here.
 
+# 首頁
 def index(request):
     return render(request, "index.html")
 
